34dce655220d42f4a5f73f257eaf48a24f8358f7
alex <ltaspro@mail.ru>
2016-01-14 21:48:11 +0200

    4-section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by LtaS on 27.12.2015.
+ */
+// Can also be used with $(document).ready()
+// slider
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+});
